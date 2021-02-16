14e96c9324595990f7e1871c48f61183aae6e8ee
Grigoriy <mikephoto9.3@gmail.com>
2021-02-16 17:24:32 +0300

    added files how its been in lessons(it-kamasutra(14-lesson)

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://i2.wp.com/sunflowerstateradio.com/wp-content/uploads/2020/05/sunflower-logo.png?fit=1898%2C1920&ssl=1' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div className='img-container'><div className='img-content'>
+            <img className='kana' src='https://d14zyouj9r0hpo.cloudfront.net/wp-content/uploads/2020/08/image-3.jpeg' />
+        </div></div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
